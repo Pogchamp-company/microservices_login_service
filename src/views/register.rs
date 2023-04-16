a8d21a4ce73495bd841cc3acf038701e59e7b378
rusty <golovinartem2004@gmail.com>
2023-04-16 18:18:33 +0400

    Fixed response and request structures names in registration

--- src/views/register.rs
+++ src/views/register.rs
@@ -7,19 +7,19 @@ use crate::{models, password_utils};
 use crate::views::base::ErrorJson;
 
 #[derive(Debug, Deserialize)]
-pub struct UserLoginRequest {
+pub struct RegisterRequest {
     login: String,
     password: String,
 }
 
 #[derive(Debug, Serialize)]
-pub struct UserLoginResponse {
+pub struct RegisterResponse {
     token: String,
 }
 
 #[post("/register", format = "json", data = "<user_login>")]
-pub async fn register(user_login: Json<UserLoginRequest>,
-                  pool: &rocket::State<PgPool>) -> Result<Json<UserLoginResponse>, status::Conflict<Json<ErrorJson>>> {
+pub async fn register(user_login: Json<RegisterRequest>,
+                      pool: &rocket::State<PgPool>) -> Result<Json<RegisterResponse>, status::Conflict<Json<ErrorJson>>> {
     let hashed_password = password_utils::hash_password(&user_login.password);
     let query_result = models::user::create_user(&user_login.login.clone(), &hashed_password, pool).await;
 
@@ -29,7 +29,7 @@ pub async fn register(user_login: Json<UserLoginRequest>,
         }))));
     }
 
-    return Ok(Json(UserLoginResponse {
+    return Ok(Json(RegisterResponse {
         token: password_utils::create_jwt(&user_login.login, &hashed_password)
     }));
 }
